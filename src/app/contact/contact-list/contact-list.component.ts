0a50c1fe1c61ecdaa2315bda765021827186c2d4
VadeJK <1801063@co.saimia.fi>
2018-05-22 12:01:20 +0300

    Refactored application toolbar component

--- src/app/contact/contact-list/contact-list.component.ts
+++ src/app/contact/contact-list/contact-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {Contact} from '../contact';
 import {ContactService} from '../services/contact.service';
 import {Router} from '@angular/router';
+import {ToolbarOptions} from '../../ui/toolbar/toolbar-options';
+import {ToolbarService} from '../../ui/toolbar/toolbar.service';
 
 @Component({
   selector: 'cw-contact-list',
@@ -12,11 +14,13 @@ export class ContactListComponent implements OnInit {
 
   contacts: Contact[];
 
-  constructor(private contactService: ContactService, private router: Router) {
+  constructor(private contactService: ContactService, private router: Router, private toolbar: ToolbarService) {
     this.contacts = [];
   }
 
   ngOnInit() {
+    this.toolbar.toolbarOptions.next(new ToolbarOptions('Contacts', []));
+
     this.contactService.getContacts().subscribe(response => {
       this.contacts = response;
       console.log(this.contacts);
